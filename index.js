9505ba0add1706faaf6f791a20f3d979e29ffe61
Beridze <marinkawii@gmail.com>
2025-12-09 20:28:14 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let number ="6";
+const pi =3.14
+
+let firstname = "giorgi";
+let lastname = "beridze";
+
+console.log(firstname + lastname);
+
+let city = "Paris";
+let country = "France";
+
+// console.log( City:city, Country:country);
+
+let value = false
+let falsevalue=String(1);
+
+let value=falsevalue;
+
+let str = "7";//(7 დაწერს)
+let num = 2;// (2 დაწერს)
+console.log(str + num);//(nun რადგან მიმატებისას სტრინგი არ გადავა რიცხვში)
+console.log(str * num);//(14 დაწერს რადგან ჩვეულებრივ მოხდება გამრავლება)
